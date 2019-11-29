379f0209e7b04c1f436cb7c2f8cf66e3d242847c
EmmetBrickHacker <emmet.brickowski.masterbuilder@gmail.com>
2019-11-29 23:37:40 +0100

    one more correction

--- main.ts
+++ main.ts
@@ -25,35 +25,34 @@ namespace Otto {
     //% speed.defl=100
     //% subcategory=Geekservo
     export function continuousGeekservo(port: servoList, speed: number) : void {
-            let buf = pins.createBuffer(4);
-            if (port == 0) {
-                buf[0] = 0x03;
-            }
-            if (port == 1) {
-                buf[0] = 0x04;
-            }
-            if (port == 2) {
-                buf[0] = 0x05;
-            }
-            if (port == 3) {
-                buf[0] = 0x06;
-            }
-            if (port == 4) {
-                buf[0] = 0x07;
-            }
-            if (port == 5) {
-                buf[0] = 0x08;
-            }
-            if (port == 6) {
-                buf[0] = 0x09;
-            }
-            if (port == 7) {
-                buf[0] = 0x10;
-            }
-            buf[1] = speed;
-            buf[2] = 0;
-            buf[3] = 0;
-            pins.i2cWriteBuffer(board_address, buf);
+        let buf = pins.createBuffer(4);
+        if (port == 0) {
+            buf[0] = 0x03;
         }
+        if (port == 1) {
+            buf[0] = 0x04;
+        }
+        if (port == 2) {
+            buf[0] = 0x05;
+        }
+        if (port == 3) {
+            buf[0] = 0x06;
+        }
+        if (port == 4) {
+            buf[0] = 0x07;
+        }
+        if (port == 5) {
+            buf[0] = 0x08;
+        }
+        if (port == 6) {
+            buf[0] = 0x09;
+        }
+        if (port == 7) {
+            buf[0] = 0x10;
+        }
+        buf[1] = speed;
+        buf[2] = 0;
+        buf[3] = 0;
+        pins.i2cWriteBuffer(board_address, buf);
     }
 }
