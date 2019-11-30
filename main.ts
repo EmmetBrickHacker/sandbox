42463fd10e249929b61fe715ff10feef0217dced
Emmet BrickHacker <thomas.morta@post.cz>
2019-11-30 01:05:46 +0100

    geekservo 270

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@
 namespace Otto {
     // I2C adresses links to wuKong board
     const board_address = 0x10
-    
+
     export enum servoList {
         //% block="S0" enumval=0
         S0,
@@ -20,14 +20,14 @@ namespace Otto {
         //% block="S6" enumval=6
         S6,
         //% block="S7" enumval=7
-        S7    
+        S7
     }
 
     //% block="continuous Geekservo 9g 360° $servo run at $power \\%"
     //% power.shadow="speedPicker"
     //% power.defl=100
     //% subcategory=Geekservo
-    export function continuousGeekservo (servo: servoList, power: number) {
+    export function continuousGeekservo(servo: servoList, power: number) {
         let buf = pins.createBuffer(4);
         if (servo == 0) {
             buf[0] = 0x03;
@@ -53,10 +53,46 @@ namespace Otto {
         if (servo == 7) {
             buf[0] = 0x10;
         }
-        buf[1] = power;
+        buf[1] = Math.map(power, -100, 100, 0, 180);
         buf[2] = 0;
         buf[3] = 0;
         pins.i2cWriteBuffer(board_address, buf);
     }
-    
+
+    //% block="Geekservo 9g 270° $servo set to $angle \\°"
+    //% angle.shadow="protractorPicker""
+    //% angle.defl=135
+    //% subcategory=Geekservo
+    export function Geekservo270(servo: servoList, angle: number) {
+        let buf = pins.createBuffer(4);
+        if (servo == 0) {
+            buf[0] = 0x03;
+        }
+        if (servo == 1) {
+            buf[0] = 0x04;
+        }
+        if (servo == 2) {
+            buf[0] = 0x05;
+        }
+        if (servo == 3) {
+            buf[0] = 0x06;
+        }
+        if (servo == 4) {
+            buf[0] = 0x07;
+        }
+        if (servo == 5) {
+            buf[0] = 0x08;
+        }
+        if (servo == 6) {
+            buf[0] = 0x09;
+        }
+        if (servo == 7) {
+            buf[0] = 0x10;
+        }
+        buf[1] = Math.map(angle, 0, 270, 0, 180);
+        buf[2] = 0;
+        buf[3] = 0;
+        pins.i2cWriteBuffer(board_address, buf);
+    }
+
 }
