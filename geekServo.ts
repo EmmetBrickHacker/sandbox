35851ab507c27d0d31764f698d34dc3fa1880a2e
Emmet BrickHacker <thomas.morta@post.cz>
2019-12-02 20:21:16 +0100

    Testing class

--- geekServo.ts
+++ geekServo.ts
@@ -25,13 +25,54 @@ namespace geekservo {
     //% speed.shadow="speedPicker"
     //% speed.defl=100
     export function continuousGeekservo(speed: number, port: ports) {
-
     }
 
-}
+    //
+    // pokusná třída
+    //
+    export enum servoList {
+        //% block="S0" enumval=0
+        S0,
+        //% block="S1" enumval=1
+        S1,
+        //% block="S2" enumval=2
+        S2,
+        //% block="S3" enumval=3
+        S3,
+        //% block="S4" enumval=4
+        S4,
+        //% block="S5" enumval=5
+        S5,
+        //% block="S6" enumval=6
+        S6,
+        //% block="S7" enumval=7
+        S7
+    }
 
-namespace Otto {
-    export function pokus () {
+    export class joint {
+        private name: string;
+        public constructor(name: string) { this.name = name; }
+        public port: servoList;
+        public position: number;
+        public defaultPosition: number;
+
+        portToString() {
+            return convertToText(this.port);
+        }
+    }
+
+    export let leftAnkle = new joint("leftAnkle");
+    leftAnkle.port = servoList.S3;
+    let variable = 10;
+
+    /**
+     * Just for testing
+     */
+    //% block="Choose port $port || $numero"
+    //% expandableArgumentMode="toggle"
+    export function choosePort(port: servoList, numero?: number) {
+        basic.showNumber(variable)
+        basic.showString(leftAnkle.portToString())
     }
 
 }
